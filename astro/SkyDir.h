65ad44110f64cfc8f86eece4190d9a01614cb47b
srobinsn <>
2002-08-13 10:01:19 +0000

    much fixing, streamlining and testing of skydir, and additional tests in test.cxx.

--- astro/SkyDir.h
+++ astro/SkyDir.h
@@ -1,4 +1,4 @@
-// $Header: /nfs/slac/g/glast/ground/cvs/Event/Event/Utilities/SkyDir.h,v 1.3 2002/08/12 20:20:39 srobinsn Exp $
+// $Header: /nfs/slac/g/glast/ground/cvs/astro/astro/SkyDir.h,v 1.1.1.1 2002/08/13 00:20:45 burnett Exp $
 #ifndef OrbitModel_SkyDir_H
 #define OrbitModel_SkyDir_H
 
@@ -8,49 +8,48 @@
 #include "CLHEP/Vector/ThreeVector.h"
 
 namespace astro {
-
-
+    
+    
 /** @class SkyDir
-  * @brief Describe an absolute direction
-  * @author S. Robinson 
-  * <br>$Id:$
-  *
-  */
-class SkyDir
-{
-public:
-    enum CoordSystem { 
-        GALACTIC,  //!  fixed direction with respect to the galactic coordinate system (l,b)
-        CELESTIAL //! fixed direction with respect to the celestial coordinate system (ra,dec) in the J2000 epoch.
+* @brief Describe an absolute direction
+* @author S. Robinson 
+* <br>$Id: SkyDir.h,v 1.1.1.1 2002/08/13 00:20:45 burnett Exp $
+*
+    */
+    class SkyDir
+    {
+    public:
+        enum CoordSystem { 
+            GALACTIC,  //!  fixed direction with respect to the galactic coordinate system (l,b)
+                CELESTIAL //! fixed direction with respect to the celestial coordinate system (ra,dec) in the J2000 epoch.
+        };
+        ///Constructors
+        ///(l,b) or (Ra, Dec) instantiation
+        SkyDir(double param1=0, double param2=0, CoordSystem inputType = CELESTIAL);
+        SkyDir(Hep3Vector);
+        
+        ///return methods
+        Hep3Vector& operator () () {return m_dir;}
+        double l () const;
+        double b () const;
+        double ra () const;
+        double dec () const;
+        const Hep3Vector& dir () const {return m_dir;}
+        
+        //!to return the opening angle (in radians) between two objects:
+        double SkyDir::difference(const SkyDir& other)const;
+        
+    private:
+        static HepRotation s_celestialToGalactic;
+        
+        Hep3Vector m_dir;
+        std::pair<double,double> setGalCoordsFromDir() const;
+        
     };
-    ///Constructors
-    ///(l,b) or (Ra, Dec) instantiation
-    SkyDir(double param1=0, double param2=0, CoordSystem inputType = CELESTIAL);
-    SkyDir(Hep3Vector);
     
-    ///return methods
-    Hep3Vector& operator () () {return m_dir;}
-    double l () const;
-    double b () const;
-    double ra () const;
-    double dec () const;
-    const Hep3Vector& dir () const {return m_dir;}
+    inline double SkyDir::difference(const SkyDir& other)const{
+        return 2.*asin(0.5*(m_dir-other.dir()).mag());
+    }
     
-    //!to return the opening angle (in radians) between two objects:
-    double SkyDir::difference(const SkyDir& other)const;
-
-    static const HepRotation& celToGal();
-private:
-    static HepRotation s_celestialToGalactic;
-
-    Hep3Vector m_dir;
-    std::pair<double,double> setGalCoordsFromDir() const;
-
-};
-
-inline double SkyDir::difference(const SkyDir& other)const{
-    return 2.*asin(0.5*(m_dir-other.dir()).mag());
-}
-
 } // namespace astro
 #endif    // LHCBEVENT_SKYDIR_H
