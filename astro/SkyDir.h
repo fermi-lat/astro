ac935ff1b11d826b2ff236ef54a0b0926bfe886d
hierath <>
2004-01-21 09:59:20 +0000

    Added project member function to allow different mapping projections.

--- astro/SkyDir.h
+++ astro/SkyDir.h
@@ -1,4 +1,4 @@
-// $Header: /nfs/slac/g/glast/ground/cvs/astro/astro/SkyDir.h,v 1.8 2003/05/08 23:40:15 pln Exp $
+// $Header: /nfs/slac/g/glast/ground/cvs/astro/astro/SkyDir.h,v 1.9 2003/06/06 20:16:49 burnett Exp $
 #ifndef OrbitModel_SkyDir_H
 #define OrbitModel_SkyDir_H
 
@@ -15,9 +15,11 @@ namespace astro {
 /** @class SkyDir
 * @brief Describe an absolute direction
 * @author S. Robinson 
-* <br>$Id: SkyDir.h,v 1.8 2003/05/08 23:40:15 pln Exp $
+* <br>$Id: SkyDir.h,v 1.9 2003/06/06 20:16:49 burnett Exp $
 *
     */
+
+   
     class SkyDir
     {
     public:
@@ -25,6 +27,9 @@ namespace astro {
             GALACTIC=0,  //!  fixed direction with respect to the galactic coordinate system (l,b)
                 CELESTIAL=1, EQUATORIAL=1 //! fixed direction with respect to the celestial coordinate system (ra,dec) in the J2000 epoch.
         };
+
+        typedef enum { CAR, SIN, TAN, ARC, NCP, GLS, MER, AIT, STG } ProjType; 
+
         ///Constructors
         ///(l,b) or (Ra, Dec) instantiation
         SkyDir(double param1=0, double param2=0, CoordSystem inputType = CELESTIAL);
@@ -43,13 +48,33 @@ namespace astro {
 
         //hammer-aitoff equal-area projection.
         std::pair<double,double> hammerAitoff()const;
-        
+
+        // Set Projection Attributes
+        void setProjection(const float ref_ra, const float ref_dec,
+                const ProjType projType, const float myRef_x, const float myRef_y, 
+                const float myScale_x, const float myScale_y, const float rot,
+                const bool use_lb);
+        // General Projection Function
+        std::pair<double,double> project() const;
     private:
         static HepRotation s_celestialToGalactic;
+
+        void initProjection(void);
         
         Hep3Vector m_dir;
 	//  std::pair<double,double> setGalCoordsFromDir() const;
 	void setGalCoordsFromDir(double&, double &) const;
+
+   bool m_project_lb;  // If true, project uses l, b coords instead of RA, DEC
+   float m_refRA;  // Projection Center RA
+   float m_refDEC; // Projection Center DEC
+   ProjType m_projType; // Projection Type.  Valid values are CAR, SIN, 
+                        // TAN, ARC, NCP, GLS, MER, AIT, STG
+   float m_refX; // Projection Output Center X
+   float m_refY; // Projection Output Center Y
+   float m_scaleX; // Projection X Scaling 1/degrees
+   float m_scaleY; // Projection Y Scaling 1/degrees
+   float m_rot;   // Projection Rotation Angle
         
     };
     
