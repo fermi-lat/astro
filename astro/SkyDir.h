98072a87f6a994aa11a9097b543c4a697e45a135
burnett <>
2004-02-05 18:46:23 +0000

    update comments

--- astro/SkyDir.h
+++ astro/SkyDir.h
@@ -1,4 +1,4 @@
-// $Header: /nfs/slac/g/glast/ground/cvs/astro/astro/SkyDir.h,v 1.10 2004/01/21 09:59:19 hierath Exp $
+// $Header: /nfs/slac/g/glast/ground/cvs/astro/astro/SkyDir.h,v 1.11 2004/02/04 23:07:55 burnett Exp $
 #ifndef OrbitModel_SkyDir_H
 #define OrbitModel_SkyDir_H
 
@@ -15,8 +15,9 @@ namespace astro {
     /** @class SkyDir
     * @brief Describe an absolute direction
     * @author S. Robinson 
-    * <br>$Id: SkyDir.h,v 1.10 2004/01/21 09:59:19 hierath Exp $
+    * <br>$Id: SkyDir.h,v 1.11 2004/02/04 23:07:55 burnett Exp $
     *
+    * Note that units associated with sky coordinates (ra, dec, l, b) are consistently in degrees
     */
 
 
@@ -34,39 +35,70 @@ namespace astro {
         ///Constructors
         ///(l,b) or (Ra, Dec) instantiation
         SkyDir(double param1=0, double param2=0, CoordSystem inputType = EQUATORIAL);
+
+        //! initialize from a vector direction
         SkyDir(Hep3Vector, CoordSystem inputType = EQUATORIAL);
 
 
-        ///return methods
+        //! function operator returns the direction
         Hep3Vector& operator () () {return m_dir;}
+        //! glactic l in degrees
         double l () const;
+        //! glactic b in degrees
         double b () const;
+        //! equatorial ra in degrees
         double ra () const;
+        //! equatorial dec in degrees
         double dec () const;
+
+        //! unit vector (in default equatorial system)
         const Hep3Vector& dir () const {return m_dir;}
 
         //!to return the opening angle (in radians) between two objects:
         double difference(const SkyDir& other)const;
 
-        //hammer-aitoff equal-area projection.
+        //!hammer-aitoff equal-area projection, scaled to fit in box with x=+-2, y=+-1.
         std::pair<double,double> hammerAitoff()const;
 
-        // Set Projection Attributes
-        static void setProjection( float ref_ra,  float ref_dec,
-             ProjType projType,  float myRef_x,  float myRef_y, 
-             float myScale_x,  float myScale_y,  float rot,
-             bool use_lb);
+        /** @brief Set Projection Attributes
 
-        /// General Projection Function: return projection, given an object
+        @param ref_ra      point of reference for the
+        @param ref_dec     projection (projection center)
+        @param projType    projection type
+        @param myRef_x     coordinates of the projection center
+        @param myRef_y           in the projection coordinate system
+        @param myScale_x   x scale of the projection coordinate system (1/degrees) at the projection center
+        @param myScale_y   y scale of the projection coordinate
+        system (1/degrees) at the projection center
+        @param rot         rotation of the projection coordinate system
+        0 = x-axis parallel to lines of equal DEC
+        M_PI = x-axis parallel to lines of equal RA
+        @param use_lb      If set to true, the projection gets the current galactic
+        coordinates instead of ra/dec.  Also it is implicit that 
+        ref_ra and ref_dec now refer to the projection center's 
+        coordinates in terms of l, b instead of ra, dec.
+        */
+
+        static void setProjection( float ref_ra,  float ref_dec,
+            ProjType projType,  float myRef_x,  float myRef_y, 
+            float myScale_x,  float myScale_y,  float rot,
+            bool use_lb);
+
+        /// General Projection Function: return projection for the object, given an object. 
+        /// depends on static projection attributes
         std::pair<double,double> project() const;
 
-        // inverse for reference
-        static int inverseProjection( float point_x, float point_y, float *point_ra, float *point_dec);
+        /** @brief inverse projection function for reference: units are degrees
+
+     	@param point_x     coordinates of the projected point
+	@param point_y          in the projection coordinate system
+	@param point_ra    ra and dec of the point to 
+	@param point_dec        be projected        
+        */
+        static int inverseProjection( double point_x, double point_y, double *point_ra, double *point_dec);
 
     private:
         static HepRotation s_equatorialToGalactic;
-
-        void initProjection(void);
 
         Hep3Vector m_dir;
         //  std::pair<double,double> setGalCoordsFromDir() const;
