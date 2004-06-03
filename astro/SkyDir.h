8dde4a2305b7ce6c83657464b1b4c597480b9a1c
hierath <>
2004-06-03 22:06:17 +0000

    Removed old projection routines

--- astro/SkyDir.h
+++ astro/SkyDir.h
@@ -1,7 +1,7 @@
 /** @file SkyDir.h
     @brief declaration of the class SkyDir
 
-   $Header: /nfs/slac/g/glast/ground/cvs/astro/astro/SkyDir.h,v 1.19 2004/05/22 02:34:16 hierath Exp $
+   $Header: /nfs/slac/g/glast/ground/cvs/astro/astro/SkyDir.h,v 1.20 2004/06/02 18:36:01 burnett Exp $
 
 */
 #ifndef OrbitModel_SkyDir_H
@@ -22,7 +22,7 @@ namespace astro {
     /** @class SkyDir
     * @brief Describe an absolute direction
     * @author S. Robinson 
-    * <br>$Id: SkyDir.h,v 1.19 2004/05/22 02:34:16 hierath Exp $
+    * <br>$Id: SkyDir.h,v 1.20 2004/06/02 18:36:01 burnett Exp $
     *
     * Note that units associated with sky coordinates (ra, dec, l, b) are consistently in degrees
     */
@@ -76,44 +76,6 @@ namespace astro {
         //!to return the opening angle (in radians) between two objects:
         double difference(const SkyDir& other)const;
 
-        //!hammer-aitoff equal-area projection, scaled to fit in box with x=+-2, y=+-1.
-        std::pair<double,double> hammerAitoff()const;
-
-        /** @brief Set Projection Attributes
-
-        @param ref_ra      point of reference for the
-        @param ref_dec     projection (projection center)
-        @param projType    projection type
-        @param myRef_x     coordinates of the projection center
-        @param myRef_y           in the projection coordinate system
-        @param myScale_x   x scale of the projection coordinate system (1/degrees) at the projection center
-        @param myScale_y   y scale of the projection coordinate
-        system (1/degrees) at the projection center
-        @param rot         rotation of the projection coordinate system
-        0 = x-axis parallel to lines of equal DEC
-        M_PI = x-axis parallel to lines of equal RA
-        @param use_lb      If set to true, the projection gets the current galactic
-        coordinates instead of ra/dec.  Also it is implicit that 
-        ref_ra and ref_dec now refer to the projection center's 
-        coordinates in terms of l, b instead of ra, dec.
-        */
-
-        static void setProjection( float ref_ra,  float ref_dec,
-            ProjType projType,  float myRef_x,  float myRef_y, 
-            float myScale_x,  float myScale_y,  float rot,
-            bool use_lb);
-
-        /// General Projection Function: return projection for the object, given an object. 
-        /// depends on static projection attributes
-
-        /** @brief version that just takes a 3-character name */
-        static void setProjection( float ref_ra,  float ref_dec,
-            const std::string& projTypeName,  float myRef_x,  float myRef_y, 
-            float myScale_x,  float myScale_y,  float rot,
-            bool use_lb);
-
-        std::pair<double,double> project() const;
-
         /** @brief Routine that gives a projection of the current coordinates 
             @param projection The projection transfomation to apply. 
             @param galactic [false] if true, generate transformation in galactic coords*/
@@ -128,18 +90,6 @@ namespace astro {
 	@param point_dec        be projected        
         */
         static int inverseProjection( double point_x, double point_y, double *point_ra, double *point_dec);
-
-        // access to all the projection coordinates
-        static bool  s_project_lb;  // If true, project uses l, b coords instead of RA, DEC
-        static float s_refRA;  // Projection Center RA
-        static float s_refDEC; // Projection Center DEC
-        static ProjType s_projType; // Projection Type.  Valid values are CAR, SIN, 
-        // TAN, ARC, NCP, GLS, MER, AIT, STG
-        static float s_refX; // Projection Output Center X
-        static float s_refY; // Projection Output Center Y
-        static float s_scaleX; // Projection X Scaling 1/degrees
-        static float s_scaleY; // Projection Y Scaling 1/degrees
-        static float s_rot;   // Projection Rotation Angle
 
         class Exception; // forward declaration
 
