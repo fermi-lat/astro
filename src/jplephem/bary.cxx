9c97fde28cae9d5465086eed07e539b0e3f5239c
burnett <>
2005-01-23 19:59:53 +0000

    implement external access to the ephemeris file,

--- src/jplephem/bary.cxx
+++ src/jplephem/bary.cxx
@@ -16,8 +16,13 @@ fitsfile *openFFile (const char *name)
    char fileName[1024];
    fitsfile *FF = NULL;
    int error = 0;
+#if 0 // first mod by T. Hierath
    const char* astro_root = ::getenv("ASTROROOT");
    sprintf(fileName, "%s/%s/%s", astro_root, "src/jplephem", name);
+#else // second mod
+      const char* extfiles_root = ::getenv("EXTFILESSYS");
+   sprintf(fileName, "%s/%s/%s", extfiles_root, "jplephem", name);
+#endif
    fits_open_file(&FF, fileName, READONLY, &error);
    if(FF == NULL)
       fprintf (stderr, "File %s could not be found in %s\n", fileName, "jplephem") ;
