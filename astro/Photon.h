46be9280a3b0110cae961b2a85bde069a000fab1
burnett <>
2006-05-15 16:00:28 +0000

    add time to photon description

--- astro/Photon.h
+++ astro/Photon.h
@@ -1,7 +1,7 @@
 /** @file Photon.h
 @brief Define the Photon class
 
-$Header$
+$Header: /nfs/slac/g/glast/ground/cvs/astro/astro/Photon.h,v 1.1 2006/03/16 04:39:02 burnett Exp $
 */
 
 #ifndef astro_Photon_h
@@ -19,17 +19,21 @@ namespace astro {
     */
     class Photon : public astro::SkyDir {
     public:
-        Photon(const astro::SkyDir& dir, double energy=0, int event_class=-1)
+        Photon(const astro::SkyDir& dir, double energy, double time, int event_class=-1)
             : astro::SkyDir(dir)
             , m_energy(energy)
+            , m_time(time)
             , m_event_class(event_class)
         {}
         double energy()const{return m_energy;}
         int eventClass()const{return m_event_class;}
+        double time()const{return m_time;}
 
     private:
         double m_energy;
+        double m_time;
         int m_event_class;
+
     };
 
 }// namespace astro
